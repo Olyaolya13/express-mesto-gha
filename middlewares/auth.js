e7b695097ed2b5af6034688cba1016e859c230da
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-09-02 18:21:00 +0300

    fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,16 +5,18 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnAuthorizedError('Необходима авторизация'));
+    next(new UnAuthorizedError('Необходима авторизация'));
+    return;
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, 'secret-key');
   } catch (err) {
-    return next(new UnAuthorizedError('Необходима авторизация'));
+    next(new UnAuthorizedError('Необходима авторизация'));
+    return;
   }
 
   req.user = payload;
