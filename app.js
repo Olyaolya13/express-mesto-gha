0d679398775c8d1ccdd8b74d80f40dbf840db894
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-09-02 20:06:08 +0300

    add pattern for avatar

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
 
 const app = express();
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
