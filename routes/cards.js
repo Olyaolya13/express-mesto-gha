f8ce86fd43870774fe01bda8502526ddceef8728
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-09-02 20:42:39 +0300

    add celebrate for card

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,31 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards, createCard, deleteCard, cardLike, deleteCardLike,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', createCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', cardLike);
-router.delete('/:cardId/likes', deleteCardLike);
+
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    link: Joi.string().pattern(/^(https?:\/\/)?[^\s/$.?#].[^\s]*$/),
+  }),
+}), createCard);
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24).required(),
+  }),
+}), deleteCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24).required(),
+  }),
+}), cardLike);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24).required(),
+  }),
+}), deleteCardLike);
 
 module.exports = router;
